d32dc6f10c5f1b4029f131961aaa9da9d5016750
HellRobotics123 <34133945+HellRobotics123@users.noreply.github.com>
2020-02-06 18:01:09 +0100

    Fiks 2
    Potensiell fiks for Ronalds problem igjen...

--- InfinteRecharge/src/main/java/frc/robot/commands/IntakeCMD.java
+++ InfinteRecharge/src/main/java/frc/robot/commands/IntakeCMD.java
@@ -7,7 +7,7 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class IntakeCMD extends Command {
   /**
