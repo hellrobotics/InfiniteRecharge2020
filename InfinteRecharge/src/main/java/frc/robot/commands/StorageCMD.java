72774fc55a6d587060ae0eeb05095f708aad8a8f
HellRobotics123 <34133945+HellRobotics123@users.noreply.github.com>
2020-02-06 17:50:07 +0100

    StorageCMD fiks?
    Potensiell fiks for Ronalds problem

--- InfinteRecharge/src/main/java/frc/robot/commands/StorageCMD.java
+++ InfinteRecharge/src/main/java/frc/robot/commands/StorageCMD.java
@@ -9,7 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class StorageCMD extends CommandBase {
+public class StorageCMD extends Command {
   /**
    * Creates a new StorageCMD.
    */
