f19b213b7a1ac78eee2ea430061356bf5922bcdd
HellRobotics123 <34133945+HellRobotics123@users.noreply.github.com>
2020-02-06 17:49:35 +0100

    DriveCMD fiks?
    Potensiell fiks for Ronalds problem

--- InfinteRecharge/src/main/java/frc/robot/commands/DriveCMD.java
+++ InfinteRecharge/src/main/java/frc/robot/commands/DriveCMD.java
@@ -9,7 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class DriveCMD extends CommandBase {
+public class DriveCMD extends Command {
   /**
    * Creates a new DriveCMD.
    */
